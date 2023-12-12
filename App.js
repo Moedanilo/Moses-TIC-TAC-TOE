1545a608de805e68cbb915f754f912d80cba3caf
Mo2 . <moses.uriarte1@gmail.com>
2023-12-11 19:45:23 -0500

    Updated our game to use JSX

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 function App() {
+
     return (
-      <div>
+    <div>  
         <h1>Tic-React-Toe</h1>
-      </div>
+        <h2>It's X's turn!</h2>
+        <div class="flex-container flex-column">
+            <div class="flex-container flex-wrap" id="board">
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+                <div class="square"></div>
+            </div>
+        <button  id='reset-button'>reset</button>
+    </div>
+    </div>
     )
 }
 
